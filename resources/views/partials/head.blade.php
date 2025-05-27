b674ccc227d39c10dadceb61886c985659e8b94a
kidaneriam fekadu <kide.fikadu1@gmail.com>
2025-05-27 14:33:08 +0300

    bug fix: Update head.blade.php to include new CSS and JS files

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,8 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
-<title>{{ $title ?? 'Laravel' }}</title>
+<title>Lersha | {{ $title ?? 'Laravel' }}</title>
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
